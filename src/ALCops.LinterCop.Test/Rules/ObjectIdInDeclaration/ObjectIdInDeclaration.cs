79f50557a95a4254ab464fbd6e925cff8c848918
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2025-12-19 21:50:36 +0100

    Add version check to skip tests for DataTypeQuery in HasDiagnostic

--- src/ALCops.LinterCop.Test/Rules/ObjectIdInDeclaration/ObjectIdInDeclaration.cs
+++ src/ALCops.LinterCop.Test/Rules/ObjectIdInDeclaration/ObjectIdInDeclaration.cs
@@ -35,6 +35,13 @@ namespace ALCops.LinterCop.Test
         [TestCase("ReportDataItem")]
         public async Task HasDiagnostic(string testCase)
         {
+            SkipTestIfVersionIsTooLow(
+                ["DataTypeQuery"],
+                testCase,
+                "13.1.16",
+                "error AL0132: 'Query' does not contain a definition for 'SaveAsJson'"
+            );
+
             var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
                 .ConfigureAwait(false);
 
