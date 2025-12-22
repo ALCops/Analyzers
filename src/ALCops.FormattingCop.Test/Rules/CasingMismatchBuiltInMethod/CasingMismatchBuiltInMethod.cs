1bf56a062676861daa1bdc6848de13146b443c50
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2025-12-22 17:10:18 +0100

    Implement "Casing Mismatch" to FormattingCop

---
+++ src/ALCops.FormattingCop.Test/Rules/CasingMismatchBuiltInMethod/CasingMismatchBuiltInMethod.cs
@@ -0,0 +1,73 @@
+using ALCops.FormattingCop.CodeFixes;
+using RoslynTestKit;
+
+namespace ALCops.FormattingCop.Test
+{
+    public class CasingMismatchBuiltInMethod : NavCodeAnalysisBase
+    {
+        private AnalyzerTestFixture _fixture;
+        private static readonly Analyzers.CasingMismatchBuiltInMethod _analyzer = new();
+        private string _testCasePath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = RoslynFixtureFactory.Create<Analyzers.CasingMismatchBuiltInMethod>();
+
+            _testCasePath = Path.Combine(
+                Directory.GetParent(
+                    Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+                    Path.Combine("Rules", nameof(CasingMismatchBuiltInMethod)));
+        }
+
+        [Test]
+        [TestCase("FieldAccess")]
+        [TestCase("GlobalReferenceExpression")]
+        [TestCase("InvocationExpression")]
+        [TestCase("LocalReferenceExpression")]
+        [TestCase("ParameterReferenceExpression")]
+        [TestCase("ReturnValueReferenceExpression")]
+        [TestCase("XmlPortDataItemAccess")]
+        public async Task HasDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.HasDiagnosticAtAllMarkers(code, DiagnosticIds.CasingMismatch);
+        }
+
+        [Test]
+        [TestCase("FieldAccess")]
+        [TestCase("GlobalReferenceExpression")]
+        [TestCase("InvocationExpression")]
+        [TestCase("LocalReferenceExpression")]
+        [TestCase("ParameterReferenceExpression")]
+        [TestCase("ReturnValueReferenceExpression")]
+        [TestCase("XmlPortDataItemAccess")]
+        public async Task NoDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(NoDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.NoDiagnosticAtAllMarkers(code, DiagnosticIds.CasingMismatch);
+        }
+
+        // [TestCase("ObjectKeyword")]
+        // public async Task HasFix(string testCase)
+        // {
+        //     var currentCode = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasFix), testCase, "current.al"))
+        //         .ConfigureAwait(false);
+
+        //     var expectedCode = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasFix), testCase, "expected.al"))
+        //         .ConfigureAwait(false);
+
+        //     var fixture = RoslynFixtureFactory.Create<CasingMismatchCodeFix>(
+        //         new CodeFixTestFixtureConfig
+        //         {
+        //             AdditionalAnalyzers = [_analyzer]
+        //         });
+
+        //     fixture.TestCodeFix(currentCode, expectedCode, DiagnosticDescriptors.CasingMismatch);
+        // }
+    }
+}
---
+++ src/ALCops.FormattingCop.Test/Rules/CasingMismatchDeclaration/CasingMismatchDeclaration.cs
@@ -0,0 +1,81 @@
+using ALCops.FormattingCop.CodeFixes;
+using RoslynTestKit;
+
+namespace ALCops.FormattingCop.Test
+{
+    public class CasingMismatchDeclaration : NavCodeAnalysisBase
+    {
+        private AnalyzerTestFixture _fixture;
+        private static readonly Analyzers.CasingMismatchDeclaration _analyzer = new();
+        private string _testCasePath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = RoslynFixtureFactory.Create<Analyzers.CasingMismatchDeclaration>();
+
+            _testCasePath = Path.Combine(
+                Directory.GetParent(
+                    Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+                    Path.Combine("Rules", nameof(CasingMismatchDeclaration)));
+        }
+
+        [Test]
+        [TestCase("DataType")]
+        [TestCase("EnumDataType")]
+        [TestCase("FieldGroup")]
+        [TestCase("LabelDataType")]
+        [TestCase("LabelProperties")]
+        [TestCase("LengthDataType")]
+        [TestCase("OptionDataType")]
+        [TestCase("Property")]
+        [TestCase("TextConstDataType")]
+        [TestCase("TriggerDeclaration")]
+        public async Task HasDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.HasDiagnosticAtAllMarkers(code, DiagnosticIds.CasingMismatch);
+        }
+
+        [Test]
+        [TestCase("AccessByPermission")]
+        [TestCase("DataType")]
+        [TestCase("EnumDataType")]
+        [TestCase("FieldGroup")]
+        [TestCase("IdentifierNameSyntaxGrouping")]
+        [TestCase("LabelDataType")]
+        [TestCase("LabelProperties")]
+        [TestCase("LengthDataType")]
+        [TestCase("OptionDataType")]
+        [TestCase("Property")]
+        [TestCase("TextConstDataType")]
+        [TestCase("TriggerDeclaration")]
+        public async Task NoDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(NoDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.NoDiagnosticAtAllMarkers(code, DiagnosticIds.CasingMismatch);
+        }
+
+        // [TestCase("ObjectKeyword")]
+        // public async Task HasFix(string testCase)
+        // {
+        //     var currentCode = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasFix), testCase, "current.al"))
+        //         .ConfigureAwait(false);
+
+        //     var expectedCode = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasFix), testCase, "expected.al"))
+        //         .ConfigureAwait(false);
+
+        //     var fixture = RoslynFixtureFactory.Create<CasingMismatchCodeFix>(
+        //         new CodeFixTestFixtureConfig
+        //         {
+        //             AdditionalAnalyzers = [_analyzer]
+        //         });
+
+        //     fixture.TestCodeFix(currentCode, expectedCode, DiagnosticDescriptors.CasingMismatch);
+        // }
+    }
+}
