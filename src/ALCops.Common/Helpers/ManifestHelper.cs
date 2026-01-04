a99d6ac2b4945bd483c86fa17fa5d5d24dae6c3d
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-01-04 16:14:48 +0100

    Fix namespace reference in GetManifest method for AppSourceCopConfiguration

--- src/ALCops.Common/Helpers/ManifestHelper.cs
+++ src/ALCops.Common/Helpers/ManifestHelper.cs
@@ -8,7 +8,7 @@ public static class ManifestHelper
     public static NavAppManifest? GetManifest(Compilation compilation)
     {
 #if NETSTANDARD2_1
-        return Microsoft.Dynamics.Nav.Analyzers.Common.AppSourceCopConfigurationProvider.GetManifest(compilation);
+        return Microsoft.Dynamics.Nav.Analyzers.Common.AppSourceCopConfiguration.AppSourceCopConfigurationProvider.GetManifest(compilation);
 #else
         return Microsoft.Dynamics.Nav.Analyzers.Common.ManifestHelper.GetManifest(compilation);
 #endif
