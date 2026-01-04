3a82dd9aa87c24ac33b9f85dd041aef0679f0979
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-01-04 19:16:17 +0100

    Add "AllowInCustomizations for omitted Fields" to ApplicationCop

---
+++ src/ALCops.ApplicationCop.Test/Rules/AllowInCustomizationsForOmittedFields/AllowInCustomizationsForOmittedFields.cs
@@ -0,0 +1,51 @@
+using RoslynTestKit;
+
+namespace ALCops.ApplicationCop.Test
+{
+    public class AllowInCustomizationsForOmittedFields : NavCodeAnalysisBase
+    {
+        private AnalyzerTestFixture _fixture;
+        private string _testCasePath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = RoslynFixtureFactory.Create<Analyzers.AllowInCustomizationsForOmittedFields>();
+
+            _testCasePath = Path.Combine(
+                Directory.GetParent(
+                    Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+                    Path.Combine("Rules", nameof(AllowInCustomizationsForOmittedFields)));
+        }
+
+        [Test]
+        [TestCase("FieldOmittedPage")]
+        [TestCase("ObsoleteStateNo")]
+        [TestCase("TableExtension")]
+        [TestCase("TableExtensionBaseDrillDownPageId")]
+        [TestCase("TableExtensionBaseLookupPageId")]
+        public async Task HasDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.HasDiagnosticAtAllMarkers(code, DiagnosticIds.AllowInCustomizationsForOmittedFields);
+        }
+
+        [Test]
+        [TestCase("AllowInCustomizationsIsSet")]
+        [TestCase("DisabledField")]
+        [TestCase("FieldOnPage")]
+        [TestCase("FieldTypeNotSupported")]
+        [TestCase("FlowFilterField")]
+        [TestCase("ObsoleteStatePending")]
+        [TestCase("TableExtension")]
+        public async Task NoDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(NoDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.NoDiagnosticAtAllMarkers(code, DiagnosticIds.AllowInCustomizationsForOmittedFields);
+        }
+    }
+}
