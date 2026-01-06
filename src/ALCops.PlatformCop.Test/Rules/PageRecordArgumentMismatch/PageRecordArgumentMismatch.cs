803ce9aa4e66d1a1f57aff729afce628053de74a
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-01-06 15:50:47 +0100

    Add version check for table extensions in HasDiagnostic test case

--- src/ALCops.PlatformCop.Test/Rules/PageRecordArgumentMismatch/PageRecordArgumentMismatch.cs
+++ src/ALCops.PlatformCop.Test/Rules/PageRecordArgumentMismatch/PageRecordArgumentMismatch.cs
@@ -32,6 +32,12 @@ namespace ALCops.PlatformCop.Test
         [TestCase("TableLookupPageId")]
         public async Task HasDiagnostic(string testCase)
         {
+            SkipTestIfVersionIsTooLow(
+                ["TableExtensionDrillDownPageId", "TableExtensionLookupPageId"],
+                testCase,
+                "13.0",
+                "No support for tableextensions when target itself is already declared in the same module");
+
             var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
                 .ConfigureAwait(false);
 
