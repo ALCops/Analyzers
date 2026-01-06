c58bc5c320a641fa7fda06f7460958e26ae72e09
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-01-06 15:31:41 +0100

    Add "PageRecordArgumentMismatch" to PlatformCop

---
+++ src/ALCops.PlatformCop.Test/Rules/PageRecordArgumentMismatch/PageRecordArgumentMismatch.cs
@@ -0,0 +1,57 @@
+using RoslynTestKit;
+
+namespace ALCops.PlatformCop.Test
+{
+    public class PageRecordArgumentMismatch : NavCodeAnalysisBase
+    {
+        private AnalyzerTestFixture _fixture;
+        private string _testCasePath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = RoslynFixtureFactory.Create<Analyzers.PageRecordArgumentMismatch>();
+
+            _testCasePath = Path.Combine(
+                Directory.GetParent(
+                    Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+                    Path.Combine("Rules", nameof(PageRecordArgumentMismatch)));
+        }
+
+        [Test]
+        [TestCase("PageGetRecord")]
+        [TestCase("PageSetRecord")]
+        [TestCase("PageSetSelectionFilter")]
+        [TestCase("PageSetTableView")]
+        [TestCase("RunPage")]
+        [TestCase("RunPageMethodAsTable")]
+        [TestCase("RunPageModal")]
+        [TestCase("TableDrillDownPageId")]
+        [TestCase("TableExtensionDrillDownPageId")]
+        [TestCase("TableExtensionLookupPageId")]
+        [TestCase("TableLookupPageId")]
+        public async Task HasDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(HasDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.HasDiagnosticAtAllMarkers(code, DiagnosticIds.PageRecordArgumentMismatch);
+        }
+
+        [Test]
+        [TestCase("RunPageTargetPageHasNoRelatedTable")]
+        [TestCase("RunPageWithMatchingSourceTable")]
+        [TestCase("RunPageWithoutRecordArg")]
+        [TestCase("TableDrillDownPageId")]
+        [TestCase("TableExtensionDrillDownPageId")]
+        [TestCase("TableExtensionLookupPageId")]
+        [TestCase("TableLookupPageId")]
+        public async Task NoDiagnostic(string testCase)
+        {
+            var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(NoDiagnostic), $"{testCase}.al"))
+                .ConfigureAwait(false);
+
+            _fixture.NoDiagnosticAtAllMarkers(code, DiagnosticIds.PageRecordArgumentMismatch);
+        }
+    }
+}
