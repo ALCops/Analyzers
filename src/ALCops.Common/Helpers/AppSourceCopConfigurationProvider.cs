b1d817cce1f9e5815486b5603515af30613d127a
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-01-13 10:07:56 +0100

    Add "InterfaceObjectNameGuide" to LinterCop

---
+++ src/ALCops.Common/Helpers/AppSourceCopConfigurationProvider.cs
@@ -0,0 +1,36 @@
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+
+namespace ALCops.Common.Extensions;
+
+public static class AppSourceCopConfigurationProvider
+{
+    public static AppSourceCopConfiguration? GetAppSourceCopConfiguration(Compilation compilation)
+    {
+        var appSourceCopConf =
+            Microsoft.Dynamics.Nav.Analyzers.Common.AppSourceCopConfiguration.AppSourceCopConfigurationProvider
+                .GetAppSourceCopConfiguration(compilation);
+
+        return AppSourceCopConfiguration.From(appSourceCopConf);
+    }
+}
+
+public sealed class AppSourceCopConfiguration
+{
+    public string[]? MandatoryAffixes { get; init; }
+    public string? MandatorySuffix { get; init; }
+    public string? MandatoryPrefix { get; init; }
+
+    internal static AppSourceCopConfiguration? From(
+        Microsoft.Dynamics.Nav.Analyzers.Common.AppSourceCopConfiguration.AppSourceCopConfiguration? appSourceCopConf)
+    {
+        if (appSourceCopConf is null)
+            return null;
+
+        return new AppSourceCopConfiguration
+        {
+            MandatoryAffixes = appSourceCopConf.MandatoryAffixes,
+            MandatorySuffix = appSourceCopConf.MandatorySuffix,
+            MandatoryPrefix = appSourceCopConf.MandatoryPrefix
+        };
+    }
+}
