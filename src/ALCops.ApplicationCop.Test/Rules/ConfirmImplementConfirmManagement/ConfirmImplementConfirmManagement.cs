fc29155d1a41f26fbc7d310bd4e15e7bb08e7401
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-05-28 09:30:49 +0200

    fix(AC0004): add version check for PageExtension support in NoDiagnostic test

--- src/ALCops.ApplicationCop.Test/Rules/ConfirmImplementConfirmManagement/ConfirmImplementConfirmManagement.cs
+++ src/ALCops.ApplicationCop.Test/Rules/ConfirmImplementConfirmManagement/ConfirmImplementConfirmManagement.cs
@@ -37,6 +37,12 @@ namespace ALCops.ApplicationCop.Test
         [TestCase("PageExtension")]
         public async Task NoDiagnostic(string testCase)
         {
+            SkipTestIfVersionIsTooLow(
+                ["PageExtension"],
+                testCase,
+                "13.0",
+                "No support for tableextensions when target itself is already declared in the same module");
+
             var code = await File.ReadAllTextAsync(Path.Combine(_testCasePath, nameof(NoDiagnostic), $"{testCase}.al"))
                 .ConfigureAwait(false);
 
